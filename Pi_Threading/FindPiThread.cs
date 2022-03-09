fa30c6025e957faa81df538c582b80717bc6d50c
Colin Weatherly <colrweat@ut.utm.edu>
2022-03-08 20:12:03 -0600

    Add project files.

---
+++ Pi_Threading/FindPiThread.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi_Threading
+{
+    public class FindPiThread
+    {
+        int DartNumber;
+        int BoardHits;
+        Random rand;
+
+        public FindPiThread(int num)
+        {
+            DartNumber = num;
+            BoardHits = 0;
+            rand = new Random();
+        }
+
+        public void ThrowDarts()
+        {
+            for (int i = 0; i < DartNumber; i++)
+            {
+                double x = rand.NextDouble();
+                double y = rand.NextDouble();
+
+                if ((x * x) + (y * y) >= 1)
+                {
+                    BoardHits++;
+                }
+            }          
+        }
+    }
+}
